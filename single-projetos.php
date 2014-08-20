2e644ec67aeb4c60b2142088e297f927c8976e7c
Diego Rojas <diego@brasa.art.br>
2014-08-20 18:41:52 -0300

    hack da single projetos e o cabecalho de situacoes

--- single-projetos.php
+++ single-projetos.php
@@ -1,20 +1,24 @@
 <?php
-$id_in = array_search('179', $post->ancestors);
-
-/*179*/
-if ( $post->post_parent == '179' || $id_in == 1 ) {
-	get_header( 'feira' );
-}
-elseif (is_single( '179' )) {
-	get_header( 'feira' );
-}
-else {
-	get_header();
-}
+	$id_in = array_search('179', $post->ancestors);
+	$id_situacoes = get_the_id();
+	
+	/*179*/
+	if ( $post->post_parent == '179' || $id_in == 179 ) {
+		get_header( 'feira' );
+	}
+	elseif (is_single( '179' )) {
+		get_header( 'feira' );
+	}
+	elseif ($id_situacoes == '660') {
+		get_header( 'situacoes' );
+	}
+	else {
+		get_header();
+	}
 ?>
 
 <?php
-if ( $post->post_parent == '179' || $id_in == 1) {
+if ( $post->post_parent == '179' || $id_in == 179) {
 	get_template_part( 'part-feira' );
 }
 elseif (is_single( '179' )) {
@@ -26,7 +30,7 @@ else {
 ?>
 
 <?php
-if ( $post->post_parent == '179' || $id_in >= 1) {
+if ( $post->post_parent == '179' || $id_in >= 179) {
 	get_footer( 'feira' );
 }
 elseif (is_single( '179' )) {
